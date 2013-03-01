0af455147c3556af2e97908451db07746e51db91
Rishi Kapadia <rishi.n.kapadia@gmail.com>
2013-02-28 21:42:15 -0800

    code written

--- NormFileName.rb
+++ NormFileName.rb
@@ -1,9 +1,33 @@
+#!/usr/bin/ruby
+
 def normalize()
 	puts "\nEnter the filepath you wish to normalize:"
 	name = gets
-	list = name.split
-	
+	list = name.split("/")
+	i = list.count - 1
 
+	while (i >= 0)
+		if (list[i] == ".")
+			list.delete_at(i)
+		elsif (list[i] == "..")
+			list.delete_at(i)
+			i -= 1
+			if (i > 0)
+				list.delete_at(i)
+			end
+		end
+		i -= 1
+	end
+	
+	name = ""
+	list.each do |i|
+		name += "/#{i}"
 	end
 
+	puts name
+
+end
+
+
 normalize()
+
